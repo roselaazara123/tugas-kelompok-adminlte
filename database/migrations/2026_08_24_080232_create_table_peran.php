29be13449b46e0cecc6888795c925b37f7f5a429
Marvel-XPPLG1 <marvelalfareza@gmail.com>
2026-08-24 15:30:50 +0700

    Database Peran Apeng

--- database/migrations/2026_08_24_080232_create_table_peran.php
+++ database/migrations/2026_08_24_080232_create_table_peran.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id(); // id INT (Primary Key, Auto Increment)
             $table->foreignId('film_id')->constrained('film')->onDelete('cascade'); // film_id INT (Foreign Key ke tabel film)
             $table->foreignId('cast_id')->constrained('cast')->onDelete('cascade'); // cast_id INT (Foreign Key ke tabel cast)
-            $table->string('nama', 50); // nama VARCHAR(45)
+            $table->string('nama', 45); // nama VARCHAR(45)
             $table->timestamps();
         });
     }
